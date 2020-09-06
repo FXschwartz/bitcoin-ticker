5a8aea54575b677adf0645f427c2a2c5ea677c76
Preston <preston.schwartz505@gmail.com>
2020-09-06 12:27:15 -0500

    Added multiple crypto options with custom currencies.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,5 @@
+import 'services/networking.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -22,10 +24,26 @@ const List<String> currenciesList = [
   'ZAR'
 ];
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '190313D7-A249-44DB-A649-CB296A3163E5';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //5: Return a Map of the results instead of a single value.
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      //Update the URL to use the crypto symbol from the cryptoList
+      String url = '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+
+      NetworkHelper networkHelper = NetworkHelper(url: url);
+
+      var bitCoinData = await networkHelper.getData();
+      double lastPrice = bitCoinData['rate'];
+      cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+    }
+    return cryptoPrices;
+  }
+}
